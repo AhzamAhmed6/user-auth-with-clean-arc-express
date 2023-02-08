81d14f2f1fd8f54a4372a25724c5020c38bc134c
ahzam <ahzamahmed6@gmail.com>
2023-02-08 18:02:19 +0500

    write test `insert user in database`
    Date:      Wed Feb 8 18:02:19 2023 +0500
    Author:    ahzamahmed6@gmail.com

--- src/use-cases/add-user.js
+++ src/use-cases/add-user.js
@@ -3,11 +3,12 @@ import makeUser from "../user";
 export default function makeAddUser({ usersDb }) {
 	return async function addUser(userInfo) {
 		const user = makeUser(userInfo);
-		const exists = await usersDb.findByEmail({ email: user.getEmail() });
+		const exists = await usersDb.findById({ id: user.getId() });
 		if (exists) {
 			return exists;
 		}
 		return usersDb.insert({
+			id: user.getId(),
 			firstName: user.getFirstName(),
 			lastName: user.getLastName(),
 			email: user.getEmail(),
---
+++ src/use-cases/add-user.test.js
@@ -0,0 +1,20 @@
+import makeAddUser from "./add-user";
+import makeUsersDb from "../data-access/user-db";
+import makeFakeUser from "../../__test__/fixtures/user";
+import makeDb from "../../__test__/fixtures/db";
+import makeUser from "../user";
+
+describe("add user", () => {
+	let usersDb;
+	beforeAll(() => (usersDb = makeUsersDb({ makeDb })));
+
+	it("inserts users in the database", async () => {
+		const fakeUser = makeFakeUser();
+		const addUser = makeAddUser({ usersDb });
+		const inserted = await addUser(fakeUser);
+		delete inserted.hashedPassword;
+		delete fakeUser.password;
+
+		expect(inserted).toMatchObject(fakeUser);
+	});
+});
