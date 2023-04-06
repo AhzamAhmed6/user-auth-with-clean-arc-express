7aa84fccdbb35c8de58f71cf6b76af1dffa02a97
ahzam <ahzamahmed6@gmail.com>
2023-04-06 23:56:36 +0500

    refactor `add-user`

--- src/use-cases/add-user.js
+++ src/use-cases/add-user.js
@@ -3,18 +3,24 @@ import makeUser from "../user/index.js";
 export default function makeAddUser({ usersDb }) {
   return async function addUser(userInfo) {
     const user = makeUser(userInfo);
-    if (
-      (await usersDb.findById({ id: user.getId() })) ||
-      (await usersDb.findByEmail({ email: user.getEmail() }))
-    ) {
+
+    const existingUserById = await usersDb.findById({ id: user.getId() });
+    const existingUserByEmail = await usersDb.findByEmail({
+      email: user.getEmail(),
+    });
+
+    if (existingUserById || existingUserByEmail) {
       throw new Error("User already registered");
     }
-    return usersDb.insert({
+
+    const newUser = {
       id: user.getId(),
       firstName: user.getFirstName(),
       lastName: user.getLastName(),
       email: user.getEmail(),
       hashedPassword: await user.getHashedPassword(),
-    });
+    };
+
+    return usersDb.insert(newUser);
   };
 }
