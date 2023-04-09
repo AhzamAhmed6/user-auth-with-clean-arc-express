2149b6294b374e719b88aee2fc42535d1f4ac657
ahzam <ahzamahmed6@gmail.com>
2023-04-09 11:18:45 +0500

    refactor: setupDb function

--- db/index.js
+++ db/index.js
@@ -2,15 +2,22 @@ import { makeDb } from "../src/data-access/index.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 (async function setupDb() {
-  console.log("Setting up database...");
-  const db = await makeDb();
-  const result = await db.collection("users").createIndexes([
-    { key: { hash: 1 }, name: "hash_idx" },
-    { key: { postId: -1 }, name: "postId_idx" },
-    { key: { replyToId: -1 }, name: "replyToId_idx" },
-  ]);
-  console.log(result);
-  console.log("Database setup complete...");
-  process.exit();
+  try {
+    console.log("Setting up database...");
+    const db = await makeDb();
+    const indexArr = [
+      { key: { hash: 1 }, name: "hash_idx" },
+      { key: { postId: -1 }, name: "postId_idx" },
+      { key: { replyToId: -1 }, name: "replyToId_idx" },
+    ];
+    const result = await db.collection("users").createIndexes(indexArr);
+    console.log(result);
+    console.log("Database setup complete...");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 })();
