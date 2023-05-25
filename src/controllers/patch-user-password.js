dcd813b1e0f3d9924c003f6ab6e2304368140f0c
ahzam <ahzamahmed6@gmail.com>
2023-05-25 21:34:21 +0500

    enhance the patch-user-password response

--- src/controllers/patch-user-password.js
+++ src/controllers/patch-user-password.js
@@ -1,6 +1,8 @@
 export default function makePatchUserPassword({
   editUserPassword,
   authorizeUser,
+  generateTokens,
+  prepareResponseBody,
   handleError,
 }) {
   return async function patchUserPassword(httpRequest) {
@@ -8,16 +10,27 @@ export default function makePatchUserPassword({
       const { oldPassword, newPassword } = httpRequest.body;
       const user = authorizeUser(httpRequest);
 
-      const patched = await editUserPassword({
+      const userInfo = await editUserPassword({
         id: user.id,
         oldPassword,
         newPassword,
       });
-      return {
-        headers: { "Content-Type": "application/json" },
-        statusCode: 200,
-        body: { patched },
-      };
+      
+      // In this section, we generate new tokens when the password is changed,
+      // as part of our strategy to invalidate all previously issued access 
+      // and refresh tokens for the user.
+      const tokens = await generateTokens(userInfo);
+
+      const { hashedPassword, ...userWithoutSensitiveData } = userInfo;
+      const responseBody = prepareResponseBody(
+        userWithoutSensitiveData,
+        tokens
+      );
+
+     const headers = { "Content-Type": "application/json" };
+      const statusCode = 200;
+      const body = responseBody;
+      return { headers, statusCode, body };
     } catch (error) {
       return handleError(error);
     }
